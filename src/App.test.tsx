9da14657afc905e0bc8e2e33490d88fca4379cea
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-02-15 14:15:11 +0100

    Setup prettier and linters

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import {App} from './App';
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
 
 test('renders my name', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Marcin Jędrzejewski/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/Marcin Jędrzejewski/i)
+  expect(linkElement).toBeInTheDocument()
+})
 
 test('renders my email address', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/jedrzejewski\.marcin@gmail\.com/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/jedrzejewski\.marcin@gmail\.com/i)
+  expect(linkElement).toBeInTheDocument()
+})
 
 test('renders my phone number', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/\+48[ ]?794[ ]?792[ ]?954/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/\+48[ ]?794[ ]?792[ ]?954/i)
+  expect(linkElement).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ export const App = () => {
       <div className="header">
         <img src="photo.png" alt="Marcin Jędrzejewski" />
         <h1>Marcin Jędrzejewski</h1>
-        <h3><a href="mailto:jedrzejewski.marcin@gmail.com">jedrzejewski.marcin@gmail.com</a></h3>
-        <h3><a href="phone:+48794792954">+48 794 792 954</a></h3>
+        <h3>
+          <a href="mailto:jedrzejewski.marcin@gmail.com">
+            jedrzejewski.marcin@gmail.com
+          </a>
+        </h3>
+        <h3>
+          <a href="phone:+48794792954">+48 794 792 954</a>
+        </h3>
       </div>
     </div>
-  );
+  )
 }
