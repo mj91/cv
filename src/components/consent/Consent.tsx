f9cb26126bdf7c447279d52297380878a8627402
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-02-23 16:11:57 +0100

    Updates

--- src/components/consent/Consent.tsx
+++
@@ -1,6 +0,0 @@
-import { useData } from '../../contexts/data'
-
-export const Consent = () => {
-  const { consent } = useData()
-  return <div>{consent}</div>
-}
--- src/contexts/language/types.ts
+++
@@ -1,9 +0,0 @@
-export type Language = 'pl' | 'en'
-
-export interface SetLanguage {
-  (laguage: Language): void
-}
-
-export interface GetLanguage {
-  (searchParams?: URL | string | null): Language
-}
