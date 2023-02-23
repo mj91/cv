476e3459e04c3a6d34e5d419c28881089dc743b8
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-02-23 17:05:06 +0100

    Fix QR so it rerender on language change

--- src/components/header/qr-code/QrCode.tsx
+++ src/components/header/qr-code/QrCode.tsx
@@ -1,12 +1,14 @@
+import { useLanguage } from 'contexts/language'
 import QRCode from 'qrcode'
 import { useState, useEffect } from 'react'
 
 export const QrCode = () => {
+  const { language } = useLanguage()
   const [qr, setQr] = useState<string>()
 
   useEffect(() => {
     QRCode.toDataURL(window.location.href).then(setQr)
-  }, [])
+  }, [language])
 
   return (
     <>
