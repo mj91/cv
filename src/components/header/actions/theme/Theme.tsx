734ddc445dae89ee39e036ec0f86a31e5765f3d2
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-02-24 12:31:49 +0100

    Save theme in local storage

--- src/components/header/actions/theme/Theme.tsx
+++ src/components/header/actions/theme/Theme.tsx
@@ -1,6 +1,16 @@
-import { useCallback } from 'react'
+import { useCookiesConsent } from 'contexts/cookies-consent/cookies-consent'
+import { useCallback, useEffect } from 'react'
 
 export const Theme = () => {
+  const { functional } = useCookiesConsent()
+
+  useEffect(() => {
+    if (functional) {
+      const theme = localStorage.getItem('theme')
+      if (theme) document.documentElement.dataset.theme = theme
+    }
+  }, [functional])
+
   const toggleTheme = useCallback(() => {
     const isDark =
       document.documentElement.dataset.theme === 'dark' ||
@@ -8,6 +18,8 @@ export const Theme = () => {
         window.matchMedia &&
         window.matchMedia('(prefers-color-scheme: dark)').matches)
     document.documentElement.dataset.theme = isDark ? 'light' : 'dark'
+
+    if (functional) localStorage.setItem('theme', isDark ? 'light' : 'dark')
   }, [])
 
   return <div onClick={toggleTheme}>🌓</div>
