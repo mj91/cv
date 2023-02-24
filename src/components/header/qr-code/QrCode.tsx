23d3c6933cc57458e4a739962933abb45fbd3a95
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-02-24 12:32:24 +0100

    Fix QR code initial render

--- src/components/header/qr-code/QrCode.tsx
+++ src/components/header/qr-code/QrCode.tsx
@@ -3,11 +3,13 @@ import { useState, useEffect, useCallback } from 'react'
 
 export const QrCode = () => {
   const [qr, setQr] = useState<string>()
+
   const generateQrCode = useCallback(async () => {
     setQr(await QRCode.toDataURL(window.location.href))
   }, [])
 
   useEffect(() => {
+    generateQrCode()
     addEventListener('beforeprint', generateQrCode)
     return () => removeEventListener('beforeprint', generateQrCode)
   }, [generateQrCode])
